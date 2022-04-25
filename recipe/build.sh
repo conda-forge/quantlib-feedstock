8eb862e503372aa9739c783ad4d009a9a2b42384
Tim Paine <t.paine154@gmail.com>
2022-04-25 17:17:05 -0400

    try autoconf on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release     \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib     \
-      -DCMAKE_CXX_FLAGS=-O2     \
-      -DQL_BUILD_EXAMPLES=OFF   \
-      -DQL_BUILD_BENCHMARK=OFF   \
-      $SRC_DIR
+#cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release     \
+#      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#      -DCMAKE_INSTALL_LIBDIR=lib     \
+#      -DCMAKE_CXX_FLAGS=-O2     \
+#      -DQL_BUILD_EXAMPLES=OFF   \
+#      -DQL_BUILD_BENCHMARK=OFF   \
+#      $SRC_DIR
+
+
+./autogen.sh
+./configure --disable-static CXXFLAGS="-O2 -g0 -Wall -Wno-unknown-pragmas -Werror" --prefix $PREFIX
+make
 make install
