8535746c443ec6d6f30677c1385603cfdabba0d1
Tim Paine <t.paine154@gmail.com>
2022-04-28 21:55:08 -0400

    update include

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,6 @@
 @REM nmake install
 @REM if errorlevel 1 exit 1
 
-msbuild QuantLib.sln /m /p:Configuration=Release
+msbuild QuantLib.sln /m /p:Configuration=Release /p:IncludePath="C:\Miniconda\include;$(IncludePath)"
 
 .\test-suite\bin\QuantLib-test-suite-x64-mt.exe --log_level=message --build_info=yes -- --faster
