a024681509a770b731cd9a023cdd21326e38d3e9
Tim Paine <t.paine154@gmail.com>
2022-04-28 23:39:00 -0400

    specify output path, make sure to build x64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 
 dir _h_env
 
-msbuild QuantLib.sln /m /p:Configuration=Release /p:IncludePath="%PREFIX%\Library\include;$(IncludePath)"
+msbuild QuantLib.sln /m /p:Configuration=Release;Platform=x64;IncludePath="%PREFIX%\Library\include;$(IncludePath)";OutDir="%PREFIX%\Library\"
 
