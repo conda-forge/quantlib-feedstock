888732c59a2f29518342ea82f64bbf1e65038284
Tim Paine <t.paine154@gmail.com>
2022-04-29 17:29:21 -0400

    remove test suite forn ow

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 copy /y %RECIPE_DIR%\Platform.cmake %SRC_DIR%\cmake
 
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_TEST_SUITE=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake
@@ -12,4 +12,4 @@ if errorlevel 1 exit 1
 dir %PREFIX\Library
 dir %PREFIX\Library\bin
 dir %PREFIX\Library\lib
-%PREFIX%\Library\bin\QuantLib-test-suite-x64-mt.exe --log_level=message --build_info=yes -- --faster  # [win]
+@REM %PREFIX%\Library\bin\QuantLib-test-suite-x64-mt.exe --log_level=message --build_info=yes -- --faster  # [win]
