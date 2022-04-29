d7ca1062855cb1d36dbc4d4238bc75762fca180a
Tim Paine <t.paine154@gmail.com>
2022-04-28 21:24:20 -0400

    switch from cmake which has dlls disabled

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,12 @@
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
-if errorlevel 1 exit 1
+@REM cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
+@REM if errorlevel 1 exit 1
 
-nmake
-if errorlevel 1 exit 1
+@REM nmake
+@REM if errorlevel 1 exit 1
 
-nmake install
-if errorlevel 1 exit 1
+@REM nmake install
+@REM if errorlevel 1 exit 1
 
+msbuild QuantLib.sln /m /p:Configuration=Release
+
+.\test-suite\bin\QuantLib-test-suite-x64-mt.exe --log_level=message --build_info=yes -- --faster
