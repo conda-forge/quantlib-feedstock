87244ebfc1132cd8c4f65395196ba41113ebe262
Tim Paine <t.paine154@gmail.com>
2022-04-29 20:43:26 -0400

    attempt a slightly modified build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,14 @@
 copy /y %RECIPE_DIR%\Platform.cmake %SRC_DIR%\cmake
 
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_TEST_SUITE=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_TEST_SUITE=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake
+cmake --build %SRC_DIR% --config Release --target ALL_BUILD
 if errorlevel 1 exit 1
 
-nmake install
+cmake --install %SRC_DIR% --config Release
 if errorlevel 1 exit 1
 
-dir %PREFIX\Library
-dir %PREFIX\Library\bin
-dir %PREFIX\Library\lib
-@REM %PREFIX%\Library\bin\QuantLib-test-suite-x64-mt.exe --log_level=message --build_info=yes -- --faster  # [win]
+mkdir -p %PREFIX%\Library\bin
+copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.dll %PREFIX%\Library\bin
+copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.exp %PREFIX%\Library\bin
