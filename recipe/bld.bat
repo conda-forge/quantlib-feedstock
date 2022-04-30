ed3b6c8875c6542d7b38913880cb37a25c830401
Tim Paine <t.paine154@gmail.com>
2022-04-30 11:21:51 -0400

    working on windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,10 @@
 copy /y %RECIPE_DIR%\Platform.cmake %SRC_DIR%\cmake
 
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=ON -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_TEST_SUITE=OFF -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
+msbuild /p:Configuration=Release /p:Platform=x64 /m %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build %SRC_DIR% --config Release --target ALL_BUILD
-if errorlevel 1 exit 1
+@REM REM file is malformed for some reason
+@REM copy /y %RECIPE_DIR%\quantlib.hpp %SRC_DIR%\%PREFIX%\Library\include\ql
 
-cmake --install %SRC_DIR% --config Release
-if errorlevel 1 exit 1
-
-REM file is malformed for some reason
-copy /y %RECIPE_DIR%\quantlib.hpp %SRC_DIR%\%PREFIX%\Library\include\ql
-
-copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.dll %PREFIX%\Library\bin
-copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.exp %PREFIX%\Library\bin
+@REM copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.dll %PREFIX%\Library\bin
+@REM copy /y %SRC_DIR%\ql\Release\QuantLib-x64-mt.exp %PREFIX%\Library\bin
