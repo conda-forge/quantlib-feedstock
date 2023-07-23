7ad3458981b1461535b45a87b3506bc7647befc0
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-07-23 17:57:46 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
-touch ql/experimental/amortizingbonds/all.hpp
-
 mkdir build
 cd build
+
+touch ql/experimental/amortizingbonds/all.hpp
 
 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D BUILD_SHARED_LIBS=OFF -D Boost_USE_STATIC_LIBS=OFF -D Boost_USE_STATIC_RUNTIME=OFF -D QL_BUILD_EXAMPLES=OFF -D QL_BUILD_TEST_SUITE=ON -D QL_BUILD_BENCHMARK=OFF %SRC_DIR%
 if errorlevel 1 exit 1
